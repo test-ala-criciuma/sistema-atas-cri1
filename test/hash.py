c354397e528799ee82c897977d7c135f57bd1c8a
Thales <thales.vaz.sousa.eng@gmail.com>
2026-01-29 18:12:55 -0300

    obra

--- test/hash.py
+++ test/hash.py
@@ -1,5 +1,5 @@
 from werkzeug.security import generate_password_hash
 
-senha_simples = 'jorge1234'
+senha_simples = 'Obra1.33@2026'
 hash_gerado = generate_password_hash(senha_simples) 
 print(hash_gerado)
